5b893522435a2104785918fd013038772608b1f2
Stefan Duerr <stefan@tech2people.at>
2024-04-11 18:30:17 +0200

    faq_reader now has chatbot functionality, the FAQ.txt was updated and properly formatted, example questions were tested

--- faq_reader.py
+++ faq_reader.py
@@ -9,6 +9,8 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from environment import Environment
 from langchain_openai import ChatOpenAI
 
+question = 'How do I plan my therapy?'
+local_env = Environment()
 
 ### PIPELINE
 
@@ -16,17 +18,13 @@ from langchain_openai import ChatOpenAI
 loader = TextLoader("./FAQ.txt")
 docs = loader.load()
 
-# llm = ChatOpenAI(model="gpt-3.5-turbo-0125")
-
-local_env = Environment()
-
 
 ## INDEXING: SPLIT
 text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=1000, chunk_overlap=200, add_start_index=True
+    chunk_size=300, chunk_overlap=20, add_start_index=True
 )
 all_splits = text_splitter.split_documents(docs)
-# print(len(all_splits), all_splits)
+# print(len(all_splits))
 
 ## INDEXING: STORE
 vectorstore = Chroma.from_documents(documents=all_splits, embedding=OpenAIEmbeddings(openai_api_version="gpt-3.5-turbo-0125", openai_api_key=local_env.OPENAI_API_KEY))
@@ -35,5 +33,34 @@ vectorstore = Chroma.from_documents(documents=all_splits, embedding=OpenAIEmbedd
 ## RETRIEVAL
 retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 6})
 
-retrieved_docs = retriever.invoke("Will there be a mobile app for abilitate?")
-print(retrieved_docs)
+retrieved_docs = retriever.invoke(question)
+
+print(question)
+print('-----------------')
+
+for page_content in retrieved_docs:
+    print(page_content)
+
+print('-----------------')
+## GENERATION
+llm = ChatOpenAI(model="gpt-3.5-turbo-0125", openai_api_key=local_env.OPENAI_API_KEY)
+
+prompt = hub.pull("rlm/rag-prompt")
+
+example_messages = prompt.invoke(
+    {"context": "filler context", "question": "filler question"}
+).to_messages()
+print(example_messages[0].content)
+
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+rag_chain = (
+    {"context": retriever | format_docs, "question": RunnablePassthrough()}
+    | prompt
+    | llm
+    | StrOutputParser()
+)
+
+for chunk in rag_chain.stream(question):
+    print(chunk, end="", flush=True)
