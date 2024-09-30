7953720417f0ce88acab92f1bcb4b5bd2fa51efa
duerrs-tech2people <126664815+duerrs-tech2people@users.noreply.github.com>
2024-09-30 18:57:47 +0200

    make repo public

--- environment.py
+++ environment.py
@@ -1,7 +1,3 @@
 class Environment:
     OPENAI_API_KEY = ''
     OPENAI_URL = 'https://api.openai.com/v1/chat/completions'
-
-    
-
-    expired_keys= ['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']
